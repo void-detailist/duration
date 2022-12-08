85daa078ad47d76a89a3cfa4d9fe0971997968f4
xenups <xenups@outlook.com>
2022-12-08 16:48:49 +0330

    add tests

---
+++ tests/test_parser.py
@@ -0,0 +1,17 @@
+from app.iso_duration import ISODuration
+
+ISODuration()
+
+
+def test_parser():
+    s = "P3Y6M4DT12H24M12S10m12n80u"
+    s2 = "PT1n"
+    s3 = "P1DT"
+    s4 = "P3Y6M4DT12H24M12S10m12n80u"
+    s5 = "P3Y6M4DT12H24M12S10m12n90u"
+
+    duration_list = [s, s2, s3, s4, s5]
+    for duration_value in duration_list:
+        d = ISODuration().pars_duration(duration_value)
+        print(d.get_seconds())
+        assert ISODuration().generate(d) == duration_value
