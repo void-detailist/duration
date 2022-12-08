f377bab09e8b4d7b817fcaebf8dff91f9804892c
xenups <xenups@outlook.com>
2022-12-08 20:13:40 +0330

    -- duration removed -- fix parse duration name -- fixed du to duration_dict

--- src/iso_duration.py
+++ src/iso_duration.py
@@ -1,7 +1,10 @@
-import re
+from __future__ import annotations
 
-from app.duration import Duration
-from app.util import convert_to_dict, rename
+import calendar
+import re
+from decimal import Decimal
+
+from src.util import convert_to_dict, rename
 
 
 class ISODuration(object):
@@ -15,6 +18,43 @@ class ISODuration(object):
         "nanoseconds": "n",
         "microseconds": "u",
     }
+
+    def __init__(self):
+        self.years: int = 0
+        self.months: int = 0
+        self.days: int = 0
+        self.hours: int = 0
+        self.minutes: int = 0
+        self.seconds: int = 0
+        self.miliseconds: int = 0
+        self.nanoseconds: int = 0
+        self.microseconds: int = 0
+
+    def _wrap(self, duration_dict: dict) -> ISODuration:
+        self.years = duration_dict.get("pY") or 0
+        self.months = duration_dict.get("pM") or 0
+        self.days = duration_dict.get("pD") or 0
+        self.hours = duration_dict.get("tH") or 0
+        self.minutes = duration_dict.get("tM") or 0
+        self.seconds = duration_dict.get("tS") or 0
+        self.miliseconds = duration_dict.get("tm") or 0
+        self.nanoseconds = duration_dict.get("tn") or 0
+        self.microseconds = duration_dict.get("tu") or 0
+        return self
+
+    def get_seconds(self) -> Decimal:
+        days_seconds = (
+            self.years * calendar.SECONDS_IN_YEAR
+            + self.months * calendar.SECONDS_IN_MONTH
+            + self.days * calendar.SECONDS_IN_DAY
+            + self.hours * calendar.SECONDS_IN_HOUR
+            + self.minutes * calendar.SECONDS_IN_MINUTE
+            + self.seconds
+            + self.miliseconds * calendar.SECONDS_IN_MILI
+            + self.microseconds * calendar.SECONDS_IN_MICRO
+            + self.nanoseconds * calendar.SECONDS_IN_NANO
+        )
+        return Decimal(days_seconds)
 
     def _is_character_valid(self, duration) -> bool:
         duration_symbols = ["P", "T", "Y", "M", "D", "H", "M", "S", "m", "u", "n"]
@@ -44,10 +84,10 @@ class ISODuration(object):
                     return convert_to_dict(match, add_letter="p")
             return {}
 
-    def pars_duration(self, duration: str) -> Duration:
+    def parse(self, duration: str):
         duration_dict = self._parse_date_duration(duration)
         duration_dict.update(self._parse_time_duration(duration))
-        return Duration(duration_dict)
+        return self._wrap(duration_dict)
 
     def _generate_date(self, du: dict) -> str:
         date_duration = rename(du, self.date_map_dict)
@@ -65,12 +105,12 @@ class ISODuration(object):
                 time_string = time_string + str(value) + str(key)
         return "T" + time_string
 
-    def _remove_date(self, du: dict) -> dict:
+    def _remove_date(self, duration_dict: dict) -> dict:
         for key, value in self.date_map_dict.items():
-            del du[key]
-        return du
+            del duration_dict[key]
+        return duration_dict
 
-    def generate(self, duration: Duration) -> str:
+    def generate(self, duration: ISODuration) -> str:
         duration_dict = duration.__dict__
         gen_date = self._generate_date(duration_dict)
         duration_dict = self._remove_date(duration_dict)
