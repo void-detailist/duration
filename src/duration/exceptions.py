06d4f708f49629d1d716e2ff2636029f9380e195
xenups <xenups@outlook.com>
2022-12-15 02:02:37 +0330

    IncorrectValue exception added

--- src/duration/exceptions.py
+++ src/duration/exceptions.py
@@ -5,3 +5,8 @@ class DurationParsingException(ValueError):
 class IncorrectPattern(DurationParsingException):
     def __init__(self):
         super().__init__("The pattern is incorrect")
+
+
+class IncorrectValue(DurationParsingException):
+    def __init__(self):
+        super().__init__("Value must be positive integer")
