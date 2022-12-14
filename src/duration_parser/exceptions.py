9eb10253d9a4802641dc5231e757596a2b3fd117
xenups <xenups@outlook.com>
2022-12-14 16:06:16 +0330

    IncorrectNumber exception removed

--- src/duration_parser/exceptions.py
+++ src/duration_parser/exceptions.py
@@ -5,8 +5,3 @@ class DurationParsingException(ValueError):
 class IncorrectPattern(DurationParsingException):
     def __init__(self):
         super().__init__("The pattern is incorrect")
-
-
-class IncorrectNumber(DurationParsingException):
-    def __init__(self):
-        super().__init__("value must be integer")
