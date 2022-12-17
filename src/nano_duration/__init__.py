1c8f73be2db0e1d4ae048e5083595f5dea5581f5
xenups <xenups@outlook.com>
2022-12-17 13:09:21 +0330

    ignore isort added

--- src/nano_duration/__init__.py
+++ src/nano_duration/__init__.py
@@ -4,9 +4,9 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(os.path.dirname(__file__))
 
-from nano_duration.exceptions import (
-    IncorrectPattern,
-    IncorrectValue,
+from nano_duration.exceptions import (  # noqa  # isort:skip
+    IncorrectPattern,  # noqa  # isort:skip
+    IncorrectValue,  # noqa  # isort:skip
 )  # noqa  # isort:skip
 from nano_duration.iso_duration import generate, parse  # noqa  # isort:skip
 from nano_duration.duration import Duration  # noqa  # isort:skip
